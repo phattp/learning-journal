a9b45474e5d43f3eb4c6b4c34ff9443747a9a80a
Phatthara Pisootrapee <phatthara.pis@gmail.com>
2025-05-01 22:55:23 +0700

    Add about me page and JavaScript

---
+++ data.js
@@ -0,0 +1,52 @@
+const blogArray = [
+  {
+    id: 0,
+    name: "Blog one",
+    desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    date: "2025-05-01",
+    image: "images/article-image-03.png",
+    altText: "A laptop with coding screen.",
+  },
+  {
+    id: 1,
+    name: "Blog two",
+    desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    date: "2025-05-01",
+    image: "images/article-image-02.png",
+    altText: "A hand holding a light bulb.",
+  },
+  {
+    id: 2,
+    name: "Blog three",
+    desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    date: "2025-05-01",
+    image: "images/article-image-01.png",
+    altText: "A laptop with web browser screen.",
+  },
+  {
+    id: 3,
+    name: "Blog four",
+    desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    date: "2025-05-01",
+    image: "images/blog-image-03.png",
+    altText: "A woman walk in desert.",
+  },
+  {
+    id: 4,
+    name: "Blog five",
+    desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    date: "2025-05-01",
+    image: "images/blog-image-01.png",
+    altText: "IDE screen with React logo.",
+  },
+  {
+    id: 5,
+    name: "Blog six",
+    desc: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+    date: "2025-05-01",
+    image: "images/blog-image-05.png",
+    altText: "Dark purple light",
+  },
+];
+
+export default blogArray;
